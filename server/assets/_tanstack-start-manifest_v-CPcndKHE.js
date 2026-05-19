ca60158e979d5667945041e3b67e91e90a38894a
Payalgoel-coder <payalgoel2708@gmail.com>
2026-05-20 04:12:44 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-CPcndKHE.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Admin/Desktop/payal-s-digital-canvas-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DDBCICNE.js"] }, "/": { filePath: "C:/Users/Admin/Desktop/payal-s-digital-canvas-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-Cr6o9lbm.js"] } }, clientEntry: "/assets/index-DDBCICNE.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-DwfDQnuU.js";
+import "node:events";
+export {
+  w as default
+};
